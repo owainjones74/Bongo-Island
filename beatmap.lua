6739fe42504bde9a6ac49e0bad8b40aa5f75eba8
Owain <owain.jones74@gmail.com>
2020-03-21 18:36:52 +0000

    beatmap table

---
+++ beatmap.lua
@@ -0,0 +1,10 @@
+
+BEATMAP = {}
+--for i=1, 100 do
+--	BEATMAP[i] = {channel = math.random(1, 4), start = i}
+--end
+
+BEATMAP[1] = {channel = math.random(1, 4), start = 4}
+BEATMAP[2] = {channel = math.random(1, 4), start = 8}
+BEATMAP[3] = {channel = math.random(1, 4), start = 12}
+BEATMAP[4] = {channel = math.random(1, 4), start = 16}
